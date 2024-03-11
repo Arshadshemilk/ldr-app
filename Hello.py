71aabf1bc5d9315b8bd887b6b36b14fd30018b52
Arshadshemilk <160718560+Arshadshemilk@users.noreply.github.com>
2024-03-11 08:10:28 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -20,27 +20,17 @@ LOGGER = get_logger(__name__)
 
 def run():
     st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+        page_title="UWLDR",
+        page_icon="🤖",
     )
 
-    st.write("# Water Leakage Detection ")
+    st.write("# ROBOTICS IN UNDERGROUND WATER LEAK DETECTION ")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        Leaks in water distribution networks (WDNs) produce significant economic losses. These leaks from underground pipelines affect the surrounding environment in different ways that can be detected
+using various technologies. This article introduces an unmanned ground vehicle (UGV) equipped with an infrared temperature sensor to remotely detect thermal anomalies on the surface caused by underground
+leaking pipelines. An MLX90614 low-cost thermopile infrared sensor was proposed to trace the surface temperature above leaking pipelines and record the corresponding position of each reading.
     """
     )
 
