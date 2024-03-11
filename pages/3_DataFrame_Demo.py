8230d85701129410e87dc93ac127fd4047a16b46
Arshadshemilk <160718560+Arshadshemilk@users.noreply.github.com>
2024-03-11 08:18:31 +0530

    Update 3_DataFrame_Demo.py

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -64,14 +64,9 @@ def data_frame_demo():
         )
 
 
-st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
-st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
-st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
-)
+st.set_page_config(page_title="DataFrame", page_icon="📊")
+st.markdown("# DataFrame")
+st.sidebar.header("DataFrame")
 
 data_frame_demo()
 
-show_code(data_frame_demo)
