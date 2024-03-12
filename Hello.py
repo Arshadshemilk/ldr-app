6c44240a580cefe7fa8622aff9ac27f5ec03e23d
Arshadshemilk <160718560+Arshadshemilk@users.noreply.github.com>
2024-03-12 15:40:28 +0000

    hi

--- Hello.py
+++ Hello.py
@@ -12,11 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import time
 import streamlit as st
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
+_text1 = """
+Leaks in water distribution networks (WDNs) produce significant economic losses. These leaks from underground pipelines affect the surrounding environment in different ways that can be detected
+using various technologies. This article introduces an unmanned ground vehicle (UGV) equipped with an infrared temperature sensor to remotely detect thermal anomalies on the surface caused by underground
+leaking pipelines. An MLX90614 low-cost thermopile infrared sensor was proposed to trace the surface temperature above leaking pipelines and record the corresponding position of each reading.
+"""
 
+
+def stream_data():
+    for word in _text1.split(" "):
+        yield word + " "
+        time.sleep(0.02)
 
 def run():
     st.set_page_config(
@@ -26,13 +37,7 @@ def run():
 
     st.write("# ROBOTICS IN UNDERGROUND WATER LEAK DETECTION ")
 
-    st.markdown(
-        """
-        Leaks in water distribution networks (WDNs) produce significant economic losses. These leaks from underground pipelines affect the surrounding environment in different ways that can be detected
-using various technologies. This article introduces an unmanned ground vehicle (UGV) equipped with an infrared temperature sensor to remotely detect thermal anomalies on the surface caused by underground
-leaking pipelines. An MLX90614 low-cost thermopile infrared sensor was proposed to trace the surface temperature above leaking pipelines and record the corresponding position of each reading.
-    """
-    )
+    st.write_stream(stream_data)
 
 
 if __name__ == "__main__":
