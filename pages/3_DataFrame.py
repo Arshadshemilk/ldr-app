08e074d65dcbb8ae71a0775978e7a2f094e3318c
Arshad shemil k <160718560+Arshadshemilk@users.noreply.github.com>
2024-03-22 23:45:03 +0530

    Update 3_DataFrame.py

--- pages/3_DataFrame.py
+++ pages/3_DataFrame.py
@@ -16,36 +16,50 @@ from urllib.error import URLError
 
 import altair as alt
 import pandas as pd
+import json
+import base64
 
 import streamlit as st
-from streamlit.hello.utils import show_code
+
+def fetch_github_json(repo_url, file_path, token):
+    try:
+        headers = {
+            'Authorization': f'token {token}',
+        }
+        api_url = f"https://api.github.com/repos/{repo_url}/contents/{file_path}"
+        response = requests.get(api_url, headers=headers)
+        
+        if response.status_code == 403:
+            st.error("GitHub API rate limit exceeded. Please try again later.")
+            return None
+        
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        content = response.json()
+        decoded_content = base64.b64decode(content['content']).decode('utf-8')
+        return decoded_content
+    except (requests.RequestException, KeyError) as e:
+        st.error(f"Failed to fetch JSON content from GitHub: {e}")
+        return None
 
 
 def data_frame_demo():
     @st.cache_data
-    def get_UN_data():
-        AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-        df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-        return df.set_index("Region")
+
 
     try:
-        df = get_UN_data()
-        countries = st.multiselect(
-            "Choose countries", list(df.index), ["China", "United States of America"]
-        )
-        if not countries:
-            st.error("Please select at least one country.")
-        else:
-            data = df.loc[countries]
-            data /= 1000000.0
-            st.write("### Gross Agricultural Production ($B)", data.sort_index())
-
-            data = data.T.reset_index()
-            data = pd.melt(data, id_vars=["index"]).rename(
-                columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
+        token = "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"
+        repo_url = "Arshadshemilk/ldr-data"
+        file_path = "gps_temp.json"
+        data = fetch_github_json(repo_url, file_path,token)
+        parsed_data = json.loads(json_data)
+        filtered_data = pd.DataFrame(parsed_data)
+        filtered_data = filtered_data.T.reset_index()
+        filtered_data = pd.meltfiltered_data, id_vars=["index"]).rename(
+            columns={"index": "logitude", "latitude": "temperature"}
             )
+        st.write(filtered_data)
             chart = (
-                alt.Chart(data)
+                alt.Chart(filtered_data)
                 .mark_area(opacity=0.3)
                 .encode(
                     x="year:T",
