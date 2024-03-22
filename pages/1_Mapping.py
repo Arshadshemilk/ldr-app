9305d6bf112124c1cb1b4123fadcda65ca3e7e04
Arshad shemil k <160718560+Arshadshemilk@users.noreply.github.com>
2024-03-22 19:02:08 +0530

    Update 1_Mapping.py

--- pages/1_Mapping.py
+++ pages/1_Mapping.py
@@ -37,7 +37,7 @@ def mapping_demo():
 
     try:
         while True:
-            json_content = fetch_json_from_github("https://raw.githubusercontent.com/Arshadshemilk/ldr-data/main/gps_temp.json")
+            json_content = fetch_json_from_github("https://raw.githubusercontent.com/Arshadshemilk/ldr-data/main/gps_temp.json?t=%s" % int(time.time()))
             st.write(json_content)
             if json_content:
                 filtered_data = pd.DataFrame(json_content)
