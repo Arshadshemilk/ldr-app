2961f0d95ab69ca4c8a2ec97070d3ca4c551bf96
Arshad shemil k <160718560+Arshadshemilk@users.noreply.github.com>
2024-04-16 06:22:43 +0000

    hi

--- pages/3_DataFrame.py
+++ pages/3_DataFrame.py
@@ -12,56 +12,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from urllib.error import URLError
-
-import altair as alt
+import firebase_admin
+from firebase_admin import credentials, db
 import pandas as pd
-import requests
-import json
-import base64
 
-import streamlit as st
+# Check if Firebase app has already been initialized
+if not firebase_admin._apps:
+    # Initialize Firebase
+    cred = credentials.Certificate("ldr-ro-firebase-adminsdk-var6u-d008f4eb5b.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://ldr-ro-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    })
 
-def fetch_github_json(repo_url, file_path, token):
-    try:
-        headers = {
-            'Authorization': f'token {token}',
-        }
-        api_url = f"https://api.github.com/repos/{repo_url}/contents/{file_path}"
-        response = requests.get(api_url, headers=headers)
-        
-        if response.status_code == 403:
-            st.error("GitHub API rate limit exceeded. Please try again later.")
-            return None
-        
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        content = response.json()
-        decoded_content = base64.b64decode(content['content']).decode('utf-8')
-        return decoded_content
-    except (requests.RequestException, KeyError) as e:
-        st.error(f"Failed to fetch JSON content from GitHub: {e}")
-        return None
-
+# Function to read Firebase data and convert it to dataframe
+def read_firebase_data():
+    ref = db.reference('/')  # Reference to the root of your Firebase database
+    data = ref.get()  # Fetch data
+    df = pd.DataFrame.from_dict(data, orient='index')
+    return df
 
 def data_frame_demo():
 
-    try:
-        token = "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"
-        repo_url = "Arshadshemilk/ldr-data"
-        file_path = "gps_temp.json"
-        data = fetch_github_json(repo_url, file_path,token)
-        parsed_data = json.loads(data)
-        filtered_data = pd.DataFrame(parsed_data)
-        filtered_data.columns = ['   Longitude   ', '   Latitude   ', '   Temperature   ']
-        st.write(filtered_data)
-    except URLError as e:
-        st.error(
-            """
-            **This demo requires internet access.**
-            Connection error: %s
-        """
-            % e.reason
-        )
+    # Read and display data
+    data = read_firebase_data()
+    st.write('Raw Data:')
+    st.write(data)
 
 
 st.set_page_config(page_title="DataFrame", page_icon="📊")
