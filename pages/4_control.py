325c32f288a5376ddfab817f5eeb6852861b1640
Arshad shemil k <160718560+Arshadshemilk@users.noreply.github.com>
2024-05-02 11:03:19 +0530

    Update 4_control.py

--- pages/4_control.py
+++ pages/4_control.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
 def main():
-    st.title("Local Webserver Inside Streamlit")
-    st.write("Below is the local webserver embedded within Streamlit:")
-    st.write("<iframe src='http://192.168.69.133' width='100%' height='500px'></iframe>", unsafe_allow_html=True)
+    st.title("Controller")
+    st.write("<iframe src='https://b1f7-117-213-20-73.ngrok-free.app' width='100%' height='500px'></iframe>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
