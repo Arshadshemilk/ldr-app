3a15d419d0082d48ae3f2fc3c11526f5e80e92c6
Arshad shemil k <160718560+Arshadshemilk@users.noreply.github.com>
2024-05-02 11:22:46 +0530

    Update 4_control.py

--- pages/4_control.py
+++ pages/4_control.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title("Controller")
-    st.write("<iframe src='https://b1f7-117-213-20-73.ngrok-free.app' width='100%' height='500px'></iframe>", unsafe_allow_html=True)
+    st.write("<iframe src='https://6c12-117-213-20-73.ngrok-free.app' width='100%' height='500px'></iframe>", unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
